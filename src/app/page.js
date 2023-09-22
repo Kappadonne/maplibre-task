d309ef3780a27d400522020b2e0494a5879921ba
Kappadonne <Maxmezwin@gmail.com>
2023-09-22 18:32:58 +0300

    Project completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,83 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useRef, useState } from "react";
+import maplibregl from "maplibre-gl";
+import classes from "./page.module.css";
+
+import data from "../../transit-stations.geojson";
 
 export default function Home() {
+  const mapPlacer = useRef(null);
+  const [mapStyle, setMapStyle] = useState("arcgis/imagery");
+  const sourceId = "transit-stations-source";
+
+  const handleMapStyleChange = (e) => {
+    setMapStyle(e.target.value);
+  };
+
+  useEffect(() => {
+    const apiKey =
+      "AAPK739f9eb83dcb4f60844b115511919e4em3epFcVYfLypOLrbrhZoNBQtY8Jc0h2Gq0FAZXzxz6oMZnixDkUoADoUKOKBlBfG";
+    const basemapStyleURL = `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/`;
+
+    const map = new maplibregl.Map({
+      container: mapPlacer.current,
+      style: `${basemapStyleURL}${mapStyle}?token=${apiKey}`,
+      center: [-106.292611313611, 31.8035654832868],
+      zoom: 10,
+    });
+
+    map.on("load", () => {
+      map.addSource(sourceId, {
+        type: "geojson",
+        data: data,
+      });
+
+      map.addLayer({
+        id: "your-fill-layer-id",
+        type: "fill",
+        source: sourceId,
+        paint: {
+          "fill-color": ["match", ["get", "type"], "local transfer center", "red", "blue"],
+          "fill-opacity": 0.7,
+        },
+      });
+    });
+
+    return () => map.remove();
+  }, [mapStyle]);
+
+  const basemapOptions = [
+    "arcgis/outdoor",
+    "arcgis/community",
+    "arcgis/navigation",
+    "arcgis/streets",
+    "arcgis/streets-relief",
+    "arcgis/imagery",
+    "arcgis/oceans",
+    "arcgis/topographic",
+    "arcgis/light-gray",
+    "arcgis/dark-gray",
+    "arcgis/human-geography",
+    "arcgis/charted-territory",
+    "arcgis/nova",
+    "osm/standard",
+    "osm/navigation",
+    "osm/streets",
+    "osm/blueprint",
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main style={{ width: "100%", height: "100vh" }}>
+      <div ref={mapPlacer} style={{ width: "100%", height: "100%" }}></div>
+      <div className={classes.basemapsWrapper}>
+        <select onChange={handleMapStyleChange} value={mapStyle} className={classes.basemaps}>
+          {basemapOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
       </div>
     </main>
-  )
+  );
 }
